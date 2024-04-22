899e3d72f46573bfcad7c6e55afde1e099114524
contug <66022609+contug@users.noreply.github.com>
2024-04-22 11:30:57 +0200

    initial commit

---
+++ src/test/java/com/example/kanbanspring/KanbanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanbanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
